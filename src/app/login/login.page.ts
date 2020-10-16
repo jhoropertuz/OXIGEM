3c538318f926a4580d78ae6fbf8d27bf5ad4877b
jhoropertuz <jhoropertuz@gmail.com>
2020-10-15 22:26:26 -0500

    vista de login terminada

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+ validations_form: FormGroup;
 
-  constructor() { }
+  constructor(public formBuilder: FormBuilder, private router: Router) { }
 
   ngOnInit() {
+
+    this.validations_form = this.formBuilder.group({
+      email: new FormControl('', Validators.compose([
+        Validators.required,
+        Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+      ])),
+      password: new FormControl('', Validators.compose([
+        Validators.required
+        //,Validators.pattern('^(?=.*[a-zA-Z])[a-zA-Z0-9!$%@#£€*?&]+$')
+      ]))
+    });
+
   }
+
+  validation_messages = {
+    'email': [
+      { type: 'required', message: 'Email es requerido.' },
+      { type: 'pattern', message: 'Email Invalido.' }
+    ],
+    'password': [
+      { type: 'required', message: 'Contraseña es requerida.' }
+     // ,{ type: 'pattern', message: 'Your password must contain uppercase, lowercase, special chars and number.' }
+    ],
+  };
+
+  onSubmitLogin(values){
+    
+    this.router.navigateByUrl("menu-principal"); // redirect page if login is successful
+  }
+
+ 
 
 }
